20386c81d1afae012dc4f38cc37152f87ec1fc05
Cédric Sougné <cedric@sougne.name>
2020-04-19 12:14:15 +0200

    53. Move Object With Mouse

---
+++ Assets/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //This field gets serialized even though it is private
+    //because it has the SerializeField attribute applied.
+    [SerializeField] private float screenWidthInUnit  = 16f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePositionXInUnit = Input.mousePosition.x / Screen.width * screenWidthInUnit;
+        Debug.Log(mousePositionXInUnit);
+        Vector2 paddlePosition = new Vector2(mousePositionXInUnit,transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
