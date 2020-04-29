dcb9c446374f0d4559bbe3cda8c5dd7567e8930b
Cédric Sougné <cedric@sougne.name>
2020-04-29 20:02:45 +0200

    63. Play Random Sound From Array

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
@@ -11,13 +13,18 @@ public class Ball : MonoBehaviour
 
     [SerializeField] private Vector2 launchVelocity = new Vector2(2f,10f);
 
+    [SerializeField] private AudioClip[] sounds;
+
     private Vector2 paddleToBallVector;
     private bool ballLaunched = false;
-    
+
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle.transform.position;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -50,5 +57,14 @@ public class Ball : MonoBehaviour
         var position = paddle.transform.position;
         Vector2 paddlePosition = new Vector2(position.x, position.y);
         transform.position = paddlePosition + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (ballLaunched)
+        {
+            AudioClip audioClip = sounds[(int)Random.Range(0, sounds.Length)];
+            audioSource.PlayOneShot(audioClip);
+        }
     }
 }
