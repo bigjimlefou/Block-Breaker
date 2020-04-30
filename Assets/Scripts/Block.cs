abfabcd06bcfe5994b9a156b8d0db7ca3b1b7d10
Cédric Sougné <cedric@sougne.name>
2020-04-30 16:54:39 +0200

    64. PlayClipAtPoint()

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip collisionSound;
+    
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
+        AudioSource.PlayClipAtPoint(collisionSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
