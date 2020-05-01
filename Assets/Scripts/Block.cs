c960c2196f26401559c078385d60cc18beb55da3
Cédric Sougné <cedric@sougne.name>
2020-05-01 14:59:12 +0200

    68. Add Player Score

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,19 +6,27 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip collisionSound;
+
+    [SerializeField]
+    private int blockScoreValue = 50;
     
     private Level level;
+    private GameStatus gameStatus;
     
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
         AudioSource.PlayClipAtPoint(collisionSound, Camera.main.transform.position);
         level.decrementBlocksCounter();
+        gameStatus.addToScore(blockScoreValue);
+        
         Destroy(gameObject);
     }
 
     private void Start()
     {
         level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
+        
         level.incrementBlocksCounter();
     }
 }
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -6,9 +6,16 @@ public class GameStatus : MonoBehaviour
 {
     [SerializeField][Range(0.1f,2f)] private float timeScale = 1f;
 
+    [SerializeField] private int score;
+
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = timeScale;
+    }
+    
+    public void addToScore(int toAdd)
+    {
+        score += toAdd;
     }
 }
