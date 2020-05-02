5014778fa2a3ef54a771919276eef9411bc50a3d
Cédric Sougné <cedric@sougne.name>
2020-05-02 17:32:18 +0200

    73. Instantiate GameObject

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,8 +7,8 @@ public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip collisionSound;
 
-    [SerializeField]
-    private int blockScoreValue = 50;
+    [SerializeField] private int blockScoreValue = 50;
+    [SerializeField] private GameObject blockSparkesVFX;
     
     private Level level;
     private GameSession _gameSession;
@@ -20,6 +20,13 @@ public class Block : MonoBehaviour
         _gameSession.addToScore(blockScoreValue);
         
         Destroy(gameObject);
+        triggerSparkesVFX();
+    }
+
+    private void triggerSparkesVFX()
+    {
+        var sparkelVFX = Instantiate(blockSparkesVFX, transform.position,transform.rotation);
+        Destroy(sparkelVFX,1f);
     }
 
     private void Start()
