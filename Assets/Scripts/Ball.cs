67f2b97f53e2a685f23211bb0d7bf537d447def0
Cédric Sougné <cedric@sougne.name>
2020-05-03 14:31:18 +0200

    78. Prevent Boring Ball Loops

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,30 +1,29 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-using Random = UnityEngine.Random;
+﻿using UnityEngine;
+using static UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
-    
-    [Tooltip("The Paddle instance to follow for start position")]
-    [SerializeField] private Paddle paddle;
-
-    [SerializeField] private Vector2 launchVelocity = new Vector2(2f,10f);
+    [Tooltip("The Paddle instance to follow for start position")] [SerializeField]
+    private Paddle paddle;
 
     [SerializeField] private AudioClip[] sounds;
+
+    private AudioSource audioSource;
+    private Rigidbody2D rigidbody2D;
+
+    [SerializeField] private Vector2 launchVelocity = new Vector2(2f, 10f);
+    [SerializeField] private float ramdomFactor = 0.2f;
 
     private Vector2 paddleToBallVector;
     private bool ballLaunched = false;
 
-    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle.transform.position;
         audioSource = GetComponent<AudioSource>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -41,15 +40,15 @@ public class Ball : MonoBehaviour
             {
                 LockBallToPaddle();
             }
-        }else
+        }
+        else
         {
         }
     }
 
     private void LaunchBallOnMouseClick()
     {
-        
-            GetComponent<Rigidbody2D>().velocity = launchVelocity;
+        rigidbody2D.velocity = launchVelocity;
     }
 
     void LockBallToPaddle()
@@ -61,10 +60,12 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        Vector2 velocityTweak = new Vector2(Range(0f, ramdomFactor), Range(0f, ramdomFactor));
         if (ballLaunched)
         {
-            AudioClip audioClip = sounds[(int)Random.Range(0, sounds.Length)];
+            AudioClip audioClip = sounds[(int) Range(0, sounds.Length)];
             audioSource.PlayOneShot(audioClip);
+            rigidbody2D.velocity += velocityTweak;
         }
     }
 }
