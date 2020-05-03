1514b291f4bc438d2c9a639f876a8752d38b0478
Cédric Sougné <cedric@sougne.name>
2020-05-03 12:44:12 +0200

    74. Using Tags In Unity

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Block : MonoBehaviour
@@ -9,31 +10,43 @@ public class Block : MonoBehaviour
 
     [SerializeField] private int blockScoreValue = 50;
     [SerializeField] private GameObject blockSparkesVFX;
-    
+
     private Level level;
     private GameSession _gameSession;
-    
+
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
         AudioSource.PlayClipAtPoint(collisionSound, Camera.main.transform.position);
-        level.decrementBlocksCounter();
-        _gameSession.addToScore(blockScoreValue);
-        
-        Destroy(gameObject);
-        triggerSparkesVFX();
+
+        if (tag == "Unbreakable")
+        {
+        }
+        else if (tag == "Breakable")
+        {
+            level.decrementBlocksCounter();
+            _gameSession.addToScore(blockScoreValue);
+            Destroy(gameObject);
+            triggerSparkesVFX();
+        }
     }
 
     private void triggerSparkesVFX()
     {
-        var sparkelVFX = Instantiate(blockSparkesVFX, transform.position,transform.rotation);
-        Destroy(sparkelVFX,1f);
+        var sparkelVFX = Instantiate(blockSparkesVFX, transform.position, transform.rotation);
+        Destroy(sparkelVFX, 1f);
     }
 
     private void Start()
     {
         level = FindObjectOfType<Level>();
         _gameSession = FindObjectOfType<GameSession>();
-        
-        level.incrementBlocksCounter();
+
+        if (tag == "Unbreakable")
+        {
+        }
+        else if (tag == "Breakable")
+        {
+            level.incrementBlocksCounter();
+        }
     }
 }
