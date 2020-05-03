8996b2c80b2da926c62e046b3baba766416fe7d7
Cédric Sougné <cedric@sougne.name>
2020-05-03 13:12:43 +0200

    75. Block Damage Levels

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,23 +7,32 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip collisionSound;
-
-    [SerializeField] private int blockScoreValue = 50;
     [SerializeField] private GameObject blockSparkesVFX;
 
     private Level level;
     private GameSession gameSession;
 
+    [SerializeField] private int hitsToBreak = 1;
+    [SerializeField] private int blockScoreValue = 50;
+    
+    [SerializeField] private int hits = 0;
+    
+
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
         AudioSource.PlayClipAtPoint(collisionSound, Camera.main.transform.position);
+        
+        hits++;
 
         if (CompareTag("Breakable"))
         {
-            level.decrementBlocksCounter();
-            gameSession.addToScore(blockScoreValue);
-            Destroy(gameObject);
-            triggerSparkesVFX();
+            if (hits >= hitsToBreak)
+            {
+                level.decrementBlocksCounter();
+                gameSession.addToScore(blockScoreValue);
+                Destroy(gameObject);
+                triggerSparkesVFX();
+            }
         }
     }
 
